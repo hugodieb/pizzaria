107f3321e99a04b032872d5ec61bfa84ddf3d72a
Hugo Dieb <hugodieb.hd@gmail.com>
2023-11-19 20:57:02 -0300

    cadastro usuario backend

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,3 +1,5 @@
+import prismaClient from '../../prisma';
+
 interface UserRequest{
   name: string;
   email: string;
@@ -6,7 +8,35 @@ interface UserRequest{
 
 class CreateUserService{
   async execute({name, email, password}: UserRequest){
-    return {name: name}
+
+    if(!email) {
+      throw new Error("Email obrigatório.")
+    }
+
+    const userAlreadyExist = await prismaClient.user.findFirst({
+      where: {
+        email: email
+      }
+    })
+
+    if(userAlreadyExist){
+      throw new Error("Usuário já existe")
+    }
+
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true
+      }
+    })
+
+    return user;
   }
 }
 
