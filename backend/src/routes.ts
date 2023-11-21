05d49510728d39b5176cf2f59aed8c9157425999
Hugo Dieb <hugodieb.hd@gmail.com>
2023-11-21 20:35:50 -0300

    implementando midlleware authtoken

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import { CreateUserController } from './controllers/user/CreateUserController'
+import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+import { isAuthenticated } from '../src/midlewares/isAuthenticated'
 
 const router = Router();
 
 router.post('/users', new CreateUserController().handle)
 
 router.post('/session', new AuthUserController().handle)
+
+router.get('/whoamy', isAuthenticated, new DetailUserController().handle)
 
 export { router }; 
