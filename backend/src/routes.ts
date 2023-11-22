09b309c427ffd23296f330df77ed7b7e793a59c4
Hugo Dieb <hugodieb.hd@gmail.com>
2023-11-22 14:22:17 -0300

    criar categoria

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,14 +2,19 @@ import { Router } from "express";
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
-import { isAuthenticated } from '../src/midlewares/isAuthenticated'
+import { isAuthenticated } from '../src/midlewares/isAuthenticated';
+import { CreateCategoryController } from '../src/controllers/category/CreateCategoryController';
 
 const router = Router();
 
-router.post('/users', new CreateUserController().handle)
+router.post('/users', new CreateUserController().handle);
 
-router.post('/session', new AuthUserController().handle)
+router.post('/session', new AuthUserController().handle);
 
-router.get('/whoamy', isAuthenticated, new DetailUserController().handle)
+router.get('/whoamy', isAuthenticated, new DetailUserController().handle);
+
+// Rotas Category
+
+router.post('/category', isAuthenticated, new CreateCategoryController().handle);
 
 export { router }; 
