39ae9632ce62c8bed927c7ccb0b68383f7dd0e20
Hugo Dieb <hugodieb.hd@gmail.com>
2023-11-27 10:55:30 -0300

    customizando componente Input

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,9 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/home.module.scss'
+import logopizzaria from '../../public/pizzaria.png'
+
+import { Input } from '../components/ui/Input'
 
 export default function Home() {
   return (
@@ -6,7 +11,22 @@ export default function Home() {
    <Head>
     <title>Pizzafun-Bem vindo!</title>
    </Head>
-   <div>Sua pizza divertida :)</div>
+   <div className={styles.containerCenter}>
+    <Image src={logopizzaria} alt='PizzariaFun' />
+
+    <div className={styles.login}>
+      <form >
+        <Input
+         placeholder='email@exemplo.com'
+         type='text'
+        />
+        <Input
+         placeholder='senha'
+         type='password'
+        />
+      </form>
+    </div>
+   </div>
    </>
   )
 }
